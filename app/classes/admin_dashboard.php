fab8cd1aa287738f68e571070dd91c9148a208ee
farahyasmine <133880469+farahyasmine@users.noreply.github.com>
2024-03-05 18:15:25 +0100

    Update admin_dashboard.php

--- app/classes/admin_dashboard.php
+++ app/classes/admin_dashboard.php
@@ -4,19 +4,41 @@ use App\Database;
 use App\Session;
 
 $session = new Session();
-
-
 $db = new Database("mysql", "root", "", "AccorEnergie");
 $pdo = $db->getPdo();
 
-$interventions = $pdo->query("SELECT interventions.*, Status.* FROM interventions JOIN Status ON interventions.ids = Status.ids;
-")->fetchAll(PDO::FETCH_ASSOC);
-$users = $pdo->query("SELECT * 444 Users")->fetchAll(PDO::FETCH_ASSOC);
+$interventionsQuery = "SELECT 
+    Interventions.*, 
+    Status.status, 
+    Urgences.urgency_level, 
+    GROUP_CONCAT(DISTINCT InterventionIntervenants.intervenant_id SEPARATOR ', ') AS intervenants_ids 
+FROM 
+    Interventions 
+JOIN 
+    Status ON Interventions.ids = Status.ids 
+JOIN 
+    Urgences ON Interventions.idu = Urgences.idu
+LEFT JOIN 
+    InterventionIntervenants ON Interventions.intervention_id = InterventionIntervenants.intervention_id 
+GROUP BY 
+    Interventions.intervention_id;";
+
+$interventions = $pdo->query($interventionsQuery)->fetchAll(PDO::FETCH_ASSOC);
+
+$users = $pdo->query("SELECT * FROM Users")->fetchAll(PDO::FETCH_ASSOC);
+
+$stmt = $pdo->query("SELECT ids, status FROM Status");
+$statuses = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+$stmt = $pdo->query("SELECT idu, urgency_level FROM urgences");
+$urgences = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 $loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/../templates');
 $twig = new \Twig\Environment($loader);
 
 echo $twig->render('admin_dashboard.html.twig', [
     'interventions' => $interventions,
-    'users' => $users
+    'statuses' => $statuses,
+    'users' => $users,
+    'urgences'=>$urgences
 ]);
